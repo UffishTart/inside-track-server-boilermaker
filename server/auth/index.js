7477c4d21a098a939a5d8e93c9269733e4faa29b
Dan Needham <dan.needham3@gmail.com>
2018-11-25 21:49:26 -0500

    janky login with fake token

--- server/auth/index.js
+++ server/auth/index.js
@@ -19,6 +19,16 @@ router.post('/login', async (req, res, next) => {
   }
 })
 
+router.post('/loginWithToken', async (req, res, next) => {
+  try {
+    const user = await User.findById(req.body.token)
+    req.login(user, err => (err ? next(err) : res.json(user)))
+  } catch (err) {
+    next(err)
+  }
+
+})
+
 router.post('/signup', async (req, res, next) => {
   try {
     const user = await User.create(req.body)
